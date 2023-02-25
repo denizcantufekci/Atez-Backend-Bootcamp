1db527ac150c2326e091a855313699413619743d
Deniz Can Tüfekçi <denizcantufekci@gmail.com>
2023-02-25 09:33:49 +0300

    add task

--- Tasks/Task_02.js
+++ Tasks/Task_02.js
@@ -7,3 +7,26 @@
     Bunun varyasyonları ile düşünüp başka koşullarda ne dönmesi gerekir, kontrolleri sağlamanız bekleniyor.
  */
 
+function multiplyOfInfinityArgs(...args){
+    if (args.length === 0) return "Please enter at least one number.";
+    if (args.some(arg => typeof arg !== "number" || isNaN(arg))) return "Please enter only numbers.";
+    let result = 1;
+    args.forEach(arg => result *= arg);
+    return result;
+}
+console.log(multiplyOfInfinityArgs());
+console.log(multiplyOfInfinityArgs(1,2,3));
+console.log(multiplyOfInfinityArgs(-1,0,3));
+console.log(multiplyOfInfinityArgs(1,2,3,4,5,Infinity));
+console.log(multiplyOfInfinityArgs(1,2,3,4,5,NaN));
+console.log(multiplyOfInfinityArgs(1,2,3,4,5,"5"));
+console.log(multiplyOfInfinityArgs(1,2,3,4,5,true));
+console.log(multiplyOfInfinityArgs(1,2,3,4,5,false));
+console.log(multiplyOfInfinityArgs(1,2,3,4,5,{name: 'x', surname: 'y'}));
+console.log(multiplyOfInfinityArgs(1,2,3,4,5,[1,2,3]));
+console.log(multiplyOfInfinityArgs(null));
+console.log(multiplyOfInfinityArgs(undefined));
+console.log(multiplyOfInfinityArgs(Infinity));
+console.log(multiplyOfInfinityArgs(1/0));
+console.log(multiplyOfInfinityArgs(1/-0));
+console.log(multiplyOfInfinityArgs(0/0));
